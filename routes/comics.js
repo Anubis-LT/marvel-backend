fec2b36369b84ac605d8327b09b00088f0730bbb
gregory Le Terte <gregory.leterte@gmail.com>
2021-02-18 18:51:49 +0100

    update request Comics limit

--- routes/comics.js
+++ routes/comics.js
@@ -6,45 +6,45 @@ const axios = require("axios");
 
 // list charaters
 router.get("/characters", async (req, res) => {
-  try {
-    // filters
-    const skip = req.query.skip;
-    const limit = 100; //req.query.limit ;
-    // requests
-    const request = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
-    );
-    res.json(request.data);
-  } catch (error) {
-    console.log("characters error", error.message);
-    res.status(400).json({ message: error.message });
-  }
+   try {
+      // filters
+      const skip = req.query.skip;
+      const limit = req.query.limit || 100; //req.query.limit ;
+      // requests
+      const request = await axios.get(
+         `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
+      );
+      res.json(request.data);
+   } catch (error) {
+      console.log("characters error", error.message);
+      res.status(400).json({ message: error.message });
+   }
 });
 
 router.get("/comics", async (req, res) => {
-  try {
-    const skip = req.query.skip;
-    const limit = req.query.limit;
-    const request = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
-    );
-    res.json(request.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+   try {
+      const skip = req.query.skip;
+      const limit = req.query.limit || 100;
+      const request = await axios.get(
+         `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
+      );
+      res.json(request.data);
+   } catch (error) {
+      res.status(400).json({ message: error.message });
+   }
 });
 
 // Search Id Character
 router.get("/comics/:Id", async (req, res) => {
-  try {
-    const characterId = req.params.characterId;
-    const request = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics/${Id}?apiKey=${process.env.API_KEY}`
-    );
-    res.json(request.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+   try {
+      const characterId = req.params.characterId;
+      const request = await axios.get(
+         `https://lereacteur-marvel-api.herokuapp.com/comics/${Id}?apiKey=${process.env.API_KEY}`
+      );
+      res.json(request.data);
+   } catch (error) {
+      res.status(400).json({ message: error.message });
+   }
 });
 
 module.exports = router;
